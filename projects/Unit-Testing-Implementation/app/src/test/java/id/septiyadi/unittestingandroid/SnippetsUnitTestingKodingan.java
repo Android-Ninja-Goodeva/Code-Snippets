fe6815c4ae7780e4a79de8f4ef21c79bb514901b
cit.septiyadi.dev@gmail.com <cit.septiyadi.dev@gmail.com>
2021-12-28 23:58:02 +0700

    Update some added to repo

--- projects/Unit-Testing-Implementation/app/src/test/java/id/septiyadi/unittestingandroid/SnippetsUnitTestingKodingan.java
+++ projects/Unit-Testing-Implementation/app/src/test/java/id/septiyadi/unittestingandroid/SnippetsUnitTestingKodingan.java
@@ -3,6 +3,7 @@ package id.septiyadi.unittestingandroid;
 import org.junit.Test;
 
 public class SnippetsUnitTestingKodingan {
+
     @Test
     public void printOutHallo() {
         System.out.println("Hallo, ini function pertama saya dalam Unit Test!");
